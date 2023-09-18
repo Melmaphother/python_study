6386f6fb700cfca91550a94470af4a7ac94367f2
Melmaphother <1559792558@qq.com>
2023-09-18 12:10:14 +0800

    modified string.py

--- string.py
+++ string.py
@@ -30,3 +30,84 @@ repr() 用于将数据转换成适合解释器阅读的字符串形式（Python 
        适合在开发和调试阶段使用；如果没有等价的语法，则会发生 SyntaxError 异常。
 """
 
+# string中的方法
+# # 长度
+print(len(str2))  # 获取字符串的长度
+print("str1 len", len(str1))
+print("str1 len(UTF-8)", len(str1.encode()))  # 默认 'UTF-8'
+print("str1 len(GBK)", len(str1.encode('GBK')))  # 按某编码进行重新编码
+
+# # 字符串split
+"""
+    str.split(sep,maxsplit)
+    str：表示要进行分割的字符串；
+    sep：用于指定分隔符，可以包含多个字符。此参数默认为 None，表示所有空字符，包括空格、换行符“\n”、制表符“\t”等。
+    maxsplit：可选参数，用于指定分割的次数，最后列表中子串的个数最多为 maxsplit+1。如果不指定或者指定为 -1，则表示分割次数没有限制。
+
+"""
+print(str2.split('e'))
+print(type(str2.split('e')))  # list
+
+# # 字符串join
+"""
+    newstr = str.join(iterable)     iterable : 可迭代的
+    split的逆过程
+    newstr：表示合并后生成的新字符串； 
+    str：用于指定合并时的分隔符；
+    iterable：做合并操作的源字符串数据，允许以列表、元组等形式提供
+"""
+dire = '', 'usr', 'bin', 'env'  # 也可以用 ('', 'usr', 'bin', 'env')
+print(type(dire))  # tuple
+print('/'.join(dire), type('/'.join(dire)))  # str
+
+# # 统计字符串个数count
+"""
+str.count(sub[,start[,end]])
+"""
+print(str2.count('e'))
+
+# # 查找字串find和rfind
+
+"""
+str.find(sub[,start[,end]])
+找不到返回-1，找到返回第一次找到的索引
+rfind从右向左索引
+"""
+print(str2.find('el'))
+print(str2.rfind('el'))
+
+# # 字符串输出左、右对齐和居中 ljust rjust center
+S1 = 'http://Melmaphother.github.io'
+S2 = 'http://Melmaphother'
+print(S1.ljust(35, '-'))
+print(S2.ljust(35, '-'))
+print(S1.rjust(35, '-'))
+print(S2.rjust(35, '-'))
+print(S1.center(35, '-'))
+print(S2.center(35, '-'))
+
+
+# # 是否以某串开头或结尾
+print('e' in S1)
+print(S1.startswith('http'))
+print(S1.endswith('io'))
+
+# # 字符串大小写转换
+"""
+    返回新的字符串
+    str.title()  将字符串中每个单词的首字母转为大写，其他字母全部转为小写
+    str.lower()  将字符串中的所有大写字母转换为小写字母
+    str.upper()  将字符串中的所有小写字母转换为大写字母
+"""
+
+
+# # 字符串去除字符串中的空格或其他字符
+
+"""
+    strip()：删除字符串前后（左右两侧）的空格或特殊字符。
+    lstrip()：删除字符串前面（左边）的空格或特殊字符。
+    rstrip()：删除字符串后面（右边）的空格或特殊字符。
+"""
+
+trip1 = '   ssss   '
+
